c0fa05e0a4122c55b912540a5769cc4b82c4c1ed
jamesjdistefano <jamesjdistefano@gmail.com>
2022-01-14 10:19:23 -0500

    Adds Gridsome site

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/logo-mad-branch-woodworking-studio.svg" />
+
+    <h1>Mad Branch Studio</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+
+</style>
